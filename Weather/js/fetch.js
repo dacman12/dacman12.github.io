fcb98e035e9f4e200e33a85437d5d9a5763e57c7
Dakota Griggs <gri13003@byui.edu>
2019-03-06 14:48:08 -0700

    added json info

--- Weather/js/fetch.js
+++ Weather/js/fetch.js
@@ -1,4 +1,4 @@
-let weatherURL = "path/to/weather.json";
+let weatherURL = "greenvilleweather.json";
 function fetchData(weatherURL){
   let cityName = 'Greenville'; // The data we want from the weather.json file
   fetch(weatherURL)
@@ -18,24 +18,41 @@ function fetchData(weatherURL){
     // ************ Get the content ******************************
 
     // Get the location data
-    let locName = "Greenville";
-    let locState = "SC";
+    let locName = g.city;
+    let locState = g.state;
     // Put them together
     let fullName = locName+', '+locState;
     // See if it worked
     console.log('fullName is: '+fullName);
+    // elevation
+    let height = Evelation;
+    console.log("I am " + height + " tall");
+    //location
+    let longitude = Longitude;
+    let lattitude = Latitude;
+    let place = longitude + ', ' + lattitude;
+    console.log("I am at " + place);
+    //zip code
+    let code = Zip;
+    console.log("I lve at " + code);    
 
     // Get the temperature data
-
+    let hi = High;
+    let low = Low;
+    let temperature = Temp;
+    let wet = Precip;
 
     // Get the wind data 
-
+    let speed = Gusts;
+    let direction = Direction;
 
     // Get the current conditions
-
+    let current = Summary;
+    console.log("It currently is " + current);
 
     // Get the hourly data 
-
+    let hour = Hourly;
+    console.log("alpha");
     // ************ Display the content ******************************
     // Set the title with the location name at the first
     // Gets the title element so it can be worked with
