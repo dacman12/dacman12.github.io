f37bf533ebf952db6a5250248f07fbc36ed9bb59
Dakota Griggs <gri13003@byui.edu>
2019-03-22 21:47:55 -0600

    local weather

--- Weather/localweather/geo_loc.js
+++ Weather/localweather/geo_loc.js
@@ -11,6 +11,8 @@
 //     //define local storage
 //     var storage = window.localStorage;
 
+console.log("test, test, mike test");
+
 // Gets longitude and latitude of current location
 function getGeoLocation() {
 
@@ -24,14 +26,19 @@ function getGeoLocation() {
             const lat = position.coords.latitude;
             const long = position.coords.longitude;
 
+            //Save coordinates individually 
+            storage.setItem("long", position.coords.longitude);
+            storage.setItem("lat", position.coords.latitude);
+
             // Combine the values
             const locale = lat + "," + long;
             console.log(`Lat and Long are: ${locale}.`);
-            console.log(`Lat and Long are: ${locale}.`);
 
+            storage.setItem("geo_loc_lat", lat);
+            storage.setItem("geo_loc_long", long);
             // Call getLocation function, send locale
             getLocation(locale);
-
+           
         })
     } else {
         status.innerHTML = "Your browser doesn't support Geolocation or it is not enabled!";
